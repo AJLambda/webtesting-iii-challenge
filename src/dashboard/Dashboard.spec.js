bc3fd6303b69062a553deb96d83398ea63c90ca9
AJLambda <apj2277@gmail.com>
2019-05-15 12:33:04 -0700

    added react-test-renderer and created snapshots

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,18 @@
 // Test away
+import React from "react";
+import renderer from "react-test-renderer"; // 1: install this npm module as a dev dependency
+
+import Dashboard from "./Dashboard";
+
+describe("<Dashboard />", () => {
+  // 2. write this test
+  it("runs the tests", () => {
+    expect(true).toBe(true);
+  });
+  it("matches snapshot", () => {
+    const tree = renderer.create(<Dashboard />); // generates a DOM tree
+
+    // snapshots are a JSON representation of the DOM tree
+    expect(tree.toJSON()).toMatchSnapshot();
+  });
+});
